c44aa764a5281567bcad7034b7b8beecc2995fdf
Tautvydas Mačiulis <maciulis.tautvydas22@gmail.com>
2021-10-31 14:44:31 +0200

    Create AppBar component, make status bar transparent

--- app/src/main/java/com/example/test_app/MainActivity.kt
+++ app/src/main/java/com/example/test_app/MainActivity.kt
@@ -3,20 +3,46 @@ package com.example.test_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.example.test_app.ui.theme.TestappTheme
 import com.example.test_app.ui.view.AppBar
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TestappTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    AppBar()
+                SetStatusBarColorTransparent()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        AppBar()
+                    },
+                    bottomBar = { Text(text = "hey")}
+                ) {
                 }
             }
         }
+    }
+}
+
+@Composable
+fun SetStatusBarColorTransparent() {
+    val systemUiController = rememberSystemUiController()
+    val useDarkIcons = MaterialTheme.colors.isLight
+
+    SideEffect {
+        systemUiController.setStatusBarColor(
+            color = Color.Transparent,
+            darkIcons = useDarkIcons
+        )
     }
 }
--- app/src/main/java/com/example/test_app/ui/view/AppBar.kt
+++ app/src/main/java/com/example/test_app/ui/view/AppBar.kt
@@ -1,20 +1,25 @@
 package com.example.test_app.ui.view
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.test_app.R
-import com.example.test_app.ui.theme.Background
 import com.example.test_app.ui.theme.TestappTheme
 
 @Composable
@@ -23,44 +28,83 @@ fun AppBar(
     onNavigationButtonClick: (() -> Unit)? = null,
     onClearButtonClick: (() -> Unit)? = null
 ) {
-    val topBarModifier = if (isRounded) 
-        Modifier.clip(
-            RoundedCornerShape(
-                bottomStart = dimensionResource(R.dimen.grid_unit_10x),
-                bottomEnd = dimensionResource(R.dimen.grid_unit_10x)
-            )
-        )
+    val showOnlyTitle = onNavigationButtonClick == null && onClearButtonClick == null
+    val cornerRadius = dimensionResource(R.dimen.grid_unit_10x)
+    val elevation = dimensionResource(R.dimen.grid_unit_8x)
+
+    val topBarModifier = if (isRounded)
+        Modifier
+            .graphicsLayer {
+                shape = RoundedCornerShape(
+                    bottomStart = cornerRadius,
+                    bottomEnd = cornerRadius
+                )
+                shadowElevation = elevation.toPx()
+                clip = true
+            }
     else
         Modifier
 
     TopAppBar(
         modifier = topBarModifier,
-        title = {
-            Image(
-                modifier = Modifier.fillMaxWidth(),
-                painter = painterResource(id = R.drawable.ic_logo),
-                contentDescription = stringResource(id = R.string.content_description_app_bar_logo)
-            )
-        },
-        navigationIcon = onNavigationButtonClick?.let {
-            {
-                IconButton(onClick = it) {
-                    Icon(
-                        Icons.Outlined.ArrowBack,
-                        stringResource(id = R.string.content_description_app_bar_navigation_icon)
-                    )
-                }
+        backgroundColor = MaterialTheme.colors.surface,
+        elevation = dimensionResource(R.dimen.grid_unit_0x)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = if (showOnlyTitle) Arrangement.Center else Arrangement.SpaceBetween
+        ) {
+            onNavigationButtonClick?.let {
+                NavigationButton(it)
             }
-        },
-        actions = onClearButtonClick?.let {
-            {
-                Text(
-                    text = stringResource(id = R.string.app_bar_clear_button_text)
-                )
+            AppBarTitle()
+            onClearButtonClick?.let {
+                ClearButton(it)
             }
-        } ?: {},
-        backgroundColor = MaterialTheme.colors.surface
+        }
+    }
+}
+
+@Composable
+fun NavigationButton(onClick: () -> Unit) {
+    IconButton(onClick = onClick) {
+        Icon(
+            Icons.Outlined.ArrowBack,
+            stringResource(id = R.string.content_description_app_bar_navigation_icon),
+            tint = MaterialTheme.colors.primary
+        )
+    }
+}
+
+@Composable
+fun AppBarTitle() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_logo),
+        contentDescription = stringResource(id = R.string.content_description_app_bar_logo)
     )
+}
+
+@Composable
+fun ClearButton(onClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxHeight()
+            .clip(CircleShape)
+            .clickable(onClick = onClick, role = Role.Button),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = stringResource(id = R.string.app_bar_clear_button_text),
+            style = MaterialTheme.typography.h3,
+            color = MaterialTheme.colors.primary
+        )
+        Icon(
+            Icons.Outlined.Delete,
+            stringResource(id = R.string.content_description_app_bar_clear_icon),
+            tint = MaterialTheme.colors.primary
+        )
+    }
 }
 
 @Preview(showBackground = true)
