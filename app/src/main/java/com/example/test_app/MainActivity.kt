f740e504aa1b28c646d29c1fb17c3773220d8e54
Tautvydas Mačiulis <maciulis.tautvydas22@gmail.com>
2021-11-01 23:12:23 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/test_app/MainActivity.kt
+++ app/src/main/java/com/example/test_app/MainActivity.kt
@@ -3,28 +3,22 @@ package com.example.test_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import com.example.test_app.feature.news.NewsViewModel
-import com.example.test_app.ui.articles.ArticlesList
 import com.example.test_app.ui.theme.TestappTheme
 import com.example.test_app.ui.view.AppBar
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.example.test_app.ui.view.TransparentStatusBar
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TestappTheme {
-                SetStatusBarColorTransparent()
-                val viewModel: NewsViewModel by viewModels()
+                TransparentStatusBar()
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
                     topBar = {
@@ -32,22 +26,8 @@ class MainActivity : ComponentActivity() {
                     },
                     bottomBar = { Text(text = "hey")}
                 ) {
-                    ArticlesList(title = "News", isLoading = true)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun SetStatusBarColorTransparent() {
-    val systemUiController = rememberSystemUiController()
-    val useDarkIcons = MaterialTheme.colors.isLight
-
-    SideEffect {
-        systemUiController.setStatusBarColor(
-            color = Color.Transparent,
-            darkIcons = useDarkIcons
-        )
     }
 }
